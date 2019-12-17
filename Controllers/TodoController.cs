47eb19860202c1589a84425371883e97ca29dd38
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2019-12-17 03:53:36 +0200

    before using database
    using interface, services and 1 inheritance file.

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index(){
-
-            return View(); //for now
+        private readonly ITodoItemService _todoItemService;
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+            return View(model);
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,7 @@ namespace AspNetCoreTodo
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+            services.AddSingleton<ITodoItemService, FakeTodoItemService>();
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
