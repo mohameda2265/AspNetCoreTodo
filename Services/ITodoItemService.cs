d9b15ec9b738e8fff3c5b585eed4025ee7d7c33d
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2019-12-24 04:33:25 +0200

    Complete items with a checkbox.
    MarkDone function added.

--- Services/ITodoItemService.cs
+++ Services/ITodoItemService.cs
@@ -9,6 +9,8 @@ namespace AspNetCoreTodo.Services
         Task<TodoItem[]> GetIncompleteItemsAsync();
 
         Task<bool> AddItemAsync(TodoItem newItem);
+
+        Task<bool> MarkDoneAsync(Guid id);
         
     }
 }
