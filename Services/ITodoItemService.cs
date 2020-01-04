a8cb390a6ce3593f6c5dca38de841bc2a267da7c
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2020-01-04 13:42:03 +0200

    .

--- Services/ITodoItemService.cs
+++ Services/ITodoItemService.cs
@@ -4,10 +4,10 @@ using System.Threading.Tasks;
 using AspNetCoreTodo.Models;
 namespace AspNetCoreTodo.Services {
     public interface ITodoItemService {
-        Task<TodoItem[]> GetIncompleteItemsAsync ();
+        Task<TodoItem[]> GetIncompleteItemsAsync (ApplicationUser user);
 
-        Task<bool> AddItemAsync (TodoItem newItem);
-
-        Task<bool> MarkDoneAsync (Guid id);
+        Task<bool> AddItemAsync (TodoItem newItem, ApplicationUser user);
+        
+        Task<bool> MarkDoneAsync (Guid id, ApplicationUser user);
     }
 }
