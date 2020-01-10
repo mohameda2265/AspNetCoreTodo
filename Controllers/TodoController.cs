f3789cbbf9013a34c2d7e1ae6c54499029e5b927
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2020-01-10 17:29:20 +0200

    Add Auth with roles

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -9,55 +9,55 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers {
-        [Authorize]
-        public class TodoController : Controller {
+    [Authorize]
+    public class TodoController : Controller {
 
-            private readonly ITodoItemService _todoItemService;
-            private readonly UserManager<ApplicationUser> _userManager;
-            public TodoController (ITodoItemService todoItemService,
-                UserManager<ApplicationUser> userManager) {
-                _todoItemService = todoItemService;
-                _userManager = userManager;
-            }
-            public async Task<IActionResult> Index () {
-                var currentUser = await _userManager.GetUserAsync (User);
-                if (currentUser == null) return Challenge ();
-                var items = await _todoItemService
-                    .GetIncompleteItemsAsync (currentUser);
-                var model = new TodoViewModel () {
-                    Items = items
-                };
-                return View (model);
-            }
-
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> AddItem (TodoItem newItem) {
-                if (!ModelState.IsValid) {
-                    return RedirectToAction ("Index");
-                }
-                var currentUser = await _userManager.GetUserAsync (User);
-                if (currentUser == null) return Challenge ();
-                var successful = await _todoItemService
-                    .AddItemAsync (newItem, currentUser);
-                if (!successful) {
-                    return BadRequest ("Could not add item.");
-                }
-                return RedirectToAction ("Index");
-            }
-
-            [ValidateAntiForgeryToken]
-            public async Task<IActionResult> MarkDone (Guid id) {
-                if (id == Guid.Empty) {
-                    return RedirectToAction ("Index");
-                }
-                var currentUser = await _userManager.GetUserAsync (User);
-                if (currentUser == null) return Challenge ();
-                var successful = await _todoItemService
-                    .MarkDoneAsync (id, currentUser);
-                if (!successful) {
-                    return BadRequest ("Could not mark item as done.");
-                }
-                return RedirectToAction ("Index");
-            }
+        private readonly ITodoItemService _todoItemService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public TodoController (ITodoItemService todoItemService,
+            UserManager<ApplicationUser> userManager) {
+            _todoItemService = todoItemService;
+            _userManager = userManager;
         }
+        public async Task<IActionResult> Index () {
+            var currentUser = await _userManager.GetUserAsync (User);
+            if (currentUser == null) return Challenge ();
+            var items = await _todoItemService
+                .GetIncompleteItemsAsync (currentUser);
+            var model = new TodoViewModel () {
+                Items = items
+            };
+            return View (model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem (TodoItem newItem) {
+            if (!ModelState.IsValid) {
+                return RedirectToAction ("Index");
+            }
+            var currentUser = await _userManager.GetUserAsync (User);
+            if (currentUser == null) return Challenge ();
+            var successful = await _todoItemService
+                .AddItemAsync (newItem, currentUser);
+            if (!successful) {
+                return BadRequest ("Could not add item.");
+            }
+            return RedirectToAction ("Index");
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone (Guid id) {
+            if (id == Guid.Empty) {
+                return RedirectToAction ("Index");
+            }
+            var currentUser = await _userManager.GetUserAsync (User);
+            if (currentUser == null) return Challenge ();
+            var successful = await _todoItemService
+                .MarkDoneAsync (id, currentUser);
+            if (!successful) {
+                return BadRequest ("Could not mark item as done.");
+            }
+            return RedirectToAction ("Index");
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -7,19 +7,37 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
-namespace AspNetCoreTodo
-{
-    public class Program
-    {
+using Microsoft.Extensions.DependencyInjection;
+namespace AspNetCoreTodo {
+    public class Program {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            InitializeDatabase(host);
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+        private static void InitializeDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+        }
     }
 }
