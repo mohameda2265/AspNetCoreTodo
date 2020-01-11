ad49870c8293ebe9be3d4a14896ebdada6abcc76
Muhammad Ashraf <33673504+mohameda2265@users.noreply.github.com>
2020-01-11 02:10:24 +0200

    fix UTesting

--- AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
+++ AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -7,11 +7,35 @@ using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace AspNetCoreTodo.UnitTests {
-    
+
     public class TodoItemServiceShould {
         [Fact]
         public async Task AddNewItemAsIncompleteWithDueDate () {
-            // ...
+            var options = new DbContextOptionsBuilder<ApplicationDbContext> ()
+                .UseInMemoryDatabase (databaseName: "Test_AddNewItem").Options;
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new ApplicationDbContext (options)) {
+                var itemsInDatabase = await context
+                    .Items.CountAsync ();
+                Assert.Equal (1, itemsInDatabase);
+                var item = await context.Items.FirstAsync ();
+                Assert.Equal ("Testing?", item.Title);
+                Assert.Equal (false, item.IsDone);
+                // Item should be due 3 days from now (give or take a second)
+                var difference = DateTimeOffset.Now.AddDays (3) - item.DueAt;
+                Assert.True (difference < TimeSpan.FromSeconds (1));
+            }
+
+            var service = new TodoItemService (context);
+            var fakeUser = new ApplicationUser {
+                Id = "fake-000",
+                UserName = "fake@example.com"
+            };
+            await service.AddItemAsync (new TodoItem {
+                Title = "Testing?"
+            }, fakeUser);
         }
+
     }
+}
 }
